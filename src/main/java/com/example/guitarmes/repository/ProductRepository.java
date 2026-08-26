4e48e5bc97b78da21a6bc15c9a0f77945b5ce568
vugeuepp <vugeuepp@gmail.com>
2026-08-26 13:46:13 +0900

    repository追加、DBカラム追加

--- src/main/java/com/example/guitarmes/repository/ProductRepository.java
+++ src/main/java/com/example/guitarmes/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.example.guitarmes.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,5 +9,23 @@ import com.example.guitarmes.entity.Product;
 
 public interface ProductRepository
         extends JpaRepository<Product, Long> {
-	List<Product> findByProductNameContaining(String keyword);
+
+    List<Product> findByProductNameContaining(
+            String keyword);
+
+    Optional<Product> findByModelNoIgnoreCase(
+            String modelNo);
+
+    boolean existsByModelNoIgnoreCase(
+            String modelNo);
+
+    boolean
+            existsByInternalModelCodeIgnoreCaseAndColorIgnoreCaseAndFingerboardMaterialIgnoreCase(
+                    String internalModelCode,
+                    String color,
+                    String fingerboardMaterial);
+
+    List<Product>
+            findByInternalModelCodeIgnoreCaseOrderByColorAscFingerboardMaterialAsc(
+                    String internalModelCode);
 }
